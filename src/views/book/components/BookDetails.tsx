a7c4f0d9d761c1f8546c6325bdbe2e28eb473d29
Daryl Boakes <darylboakes@hotmail.co.uk>
2024-10-24 20:40:33 +0100

    tweak Book folder structure

---
+++ src/views/book/components/BookDetails.tsx
@@ -0,0 +1,18 @@
+import { Text, ViewColumn } from "@/components";
+import { BookRecord } from "@/db/schemas/book";
+import { useAppTheme } from "@/styles/useAppTheme";
+
+type Props = {
+  book: BookRecord;
+};
+
+export function BookDetails({ book }: Props) {
+  const { colors, headerFontFamily } = useAppTheme();
+
+  return (
+    <ViewColumn flex={1}>
+      <Text style={{ fontFamily: headerFontFamily, fontSize: 18 }}>{book.name}</Text>
+      <Text style={{ color: colors.textSecondary }}>{book.author}</Text>
+    </ViewColumn>
+  );
+}
---
+++ src/views/book/index.tsx
@@ -0,0 +1,75 @@
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+import { Button, Text, ViewColumn, ViewRow } from "@/components";
+import { useDeleteBook } from "@/db/mutations/useDeleteBook";
+import { useGetBook } from "@/db/queries/useGetBook";
+import { useAppTheme } from "@/styles/useAppTheme";
+import { MealsTable } from "@/views/meals/components/MealsTable";
+import { EditBookModal } from "@/views/settings/Books/components/EditBookModal";
+
+import { BookDetails } from "./components/BookDetails";
+
+export function BookView() {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const { colors } = useAppTheme();
+
+  const [showEditBookModal, setShowEditBookModal] = useState(false);
+  const { data: book } = useGetBook(id);
+  const { mutate: deleteBook, isMutating: isDeleting } = useDeleteBook();
+
+  function openDeleteAlert() {
+    Alert.alert("Delete Book", "Are you sure you want to delete this book?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: handleDeleteBook,
+      },
+    ]);
+  }
+
+  async function handleDeleteBook() {
+    await deleteBook(id);
+    router.navigate("/(settings)/books");
+  }
+
+  if (!book[0]) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <>
+      <ViewColumn width="100%">
+        <ViewRow padding={16} borderBottomColor={colors.gray[5]} borderBottomWidth={1} gap={16} height="auto">
+          <BookDetails book={book[0]} />
+          <ViewColumn gap={8}>
+            <Button
+              color="success"
+              style={{ paddingVertical: 6 }}
+              textStyle={{ textAlign: "center" }}
+              onPress={() => setShowEditBookModal(true)}
+            >
+              Edit
+            </Button>
+            <Button
+              color="error"
+              style={{ paddingVertical: 6 }}
+              textStyle={{ textAlign: "center" }}
+              onPress={openDeleteAlert}
+              disabled={isDeleting}
+            >
+              Delete
+            </Button>
+          </ViewColumn>
+        </ViewRow>
+        <MealsTable bookId={id} />
+      </ViewColumn>
+      <EditBookModal show={showEditBookModal} selectedBook={book[0]} handleClose={() => setShowEditBookModal(false)} />
+    </>
+  );
+}

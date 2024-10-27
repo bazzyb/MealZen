8d4430870759c02f045d7b492825f021a4769e3b
Daryl Boakes <darylboakes@hotmail.co.uk>
2024-10-27 08:03:22 +0000

    try and reduce/eliminate database is locked issues

--- src/providers/PowerSyncProvider.tsx
+++ src/providers/PowerSyncProvider.tsx
@@ -1,6 +1,6 @@
 import "@azure/core-asynciterator-polyfill";
 import { AbstractPowerSyncDatabase, PowerSyncContext, PowerSyncDatabase, SyncStatus } from "@powersync/react-native";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Toast from "react-native-toast-message";
 
 import { LoadingSplash } from "@/components/LoadingSplash";
@@ -14,10 +14,16 @@ import { useAuth } from "./AuthProvider";
 export const PowerSyncProvider = ({ children }: { children: ReactNode }) => {
   const { isSyncEnabled } = useAuth();
   const [powerSync, setPowersync] = useState<AbstractPowerSyncDatabase | null>(null);
+  const powerSyncRef = useRef<AbstractPowerSyncDatabase | null>(null);
   const [status, setStatus] = useState<SyncStatus | null>(null);
   const [connector] = useState(supabase);
 
   const handleDBConnect = async (syncEnabled: boolean) => {
+    // ensure previous connection is closed
+    if (powerSyncRef.current) {
+      await powerSyncRef.current.close();
+    }
+
     const db = new PowerSyncDatabase({
       schema: buildSchema(syncEnabled),
       database: { dbFilename: "mealzen.db" },
@@ -59,6 +65,7 @@ export const PowerSyncProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     if (powerSync) {
+      powerSyncRef.current = powerSync;
       // getting status directly from powerSync hook leads to stale data
       // so we listen to status changes and assign to state
       powerSync.registerListener({
@@ -66,6 +73,15 @@ export const PowerSyncProvider = ({ children }: { children: ReactNode }) => {
       });
     }
   }, [powerSync]);
+
+  useEffect(() => {
+    return () => {
+      // Ensure the database connection is closed when the app is closed or hot reloads
+      if (powerSyncRef.current) {
+        powerSyncRef.current.close();
+      }
+    };
+  }, []);
 
   if (powerSync === null || status === null) {
     return <LoadingSplash />;

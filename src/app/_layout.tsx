66372712733e969dcef4001276b01c6cc464f16f
Daryl Boakes <darylboakes@hotmail.co.uk>
2024-11-13 19:55:41 +0000

    tweak styling of info and success toasts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,14 @@
 import * as SplashScreen from "expo-splash-screen";
 import { NotoSans_400Regular, NotoSans_700Bold, useFonts } from "@expo-google-fonts/noto-sans";
 import { useEffect } from "react";
-import Toast, { ErrorToast } from "react-native-toast-message";
+import Toast, { ErrorToast, InfoToast, SuccessToast } from "react-native-toast-message";
 
 import { LoadingSplash } from "@/components/LoadingSplash";
 import { AppStack } from "@/components/Navigation/AppStack";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { PowerSyncProvider } from "@/providers/PowerSyncProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import { colors } from "@/styles/colors";
 import { Logger } from "@/utils/logger";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -52,13 +53,37 @@ export default function RootLayout() {
       </ThemeProvider>
       <Toast
         config={{
+          info: props => (
+            <InfoToast
+              {...props}
+              style={{
+                height: "auto",
+                paddingVertical: 8,
+                borderLeftColor: colors.blue[6],
+              }}
+              text1NumberOfLines={2}
+              text2NumberOfLines={5}
+            />
+          ),
+          success: props => (
+            <SuccessToast
+              {...props}
+              style={{
+                height: "auto",
+                paddingVertical: 8,
+                borderLeftColor: colors.green[6],
+              }}
+              text1NumberOfLines={2}
+              text2NumberOfLines={5}
+            />
+          ),
           error: props => (
             <ErrorToast
               {...props}
               style={{
                 height: "auto",
                 paddingVertical: 8,
-                borderLeftColor: "#C11",
+                borderLeftColor: colors.red[6],
               }}
               text1NumberOfLines={2}
               text2NumberOfLines={5}

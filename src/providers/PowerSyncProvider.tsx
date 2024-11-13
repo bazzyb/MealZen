77ad1981ac15a77007eb017bca78e96443f90bd3
Daryl Boakes <darylboakes@hotmail.co.uk>
2024-11-13 19:08:26 +0000

    simplify PowerSyncProvider some more after updates

--- src/providers/PowerSyncProvider.tsx
+++ src/providers/PowerSyncProvider.tsx
@@ -1,75 +1,42 @@
 import "@azure/core-asynciterator-polyfill";
-import { AbstractPowerSyncDatabase, PowerSyncContext, PowerSyncDatabase, SyncStatus } from "@powersync/react-native";
-import { ReactNode, useEffect, useState } from "react";
-import Toast from "react-native-toast-message";
+import { PowerSyncContext, PowerSyncDatabase } from "@powersync/react-native";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 
-import { LoadingSplash } from "@/components/LoadingSplash";
 import { buildSchema } from "@/db";
 import { supabase } from "@/supabase";
-import { parseError } from "@/utils/errors";
 import { Logger } from "@/utils/logger";
 
 import { useAuth } from "./AuthProvider";
 
 export const PowerSyncProvider = ({ children }: { children: ReactNode }) => {
   const { isSyncEnabled } = useAuth();
-  const [powerSync, setPowersync] = useState<AbstractPowerSyncDatabase | null>(null);
-  const [status, setStatus] = useState<SyncStatus | null>(null);
   const [connector] = useState(supabase);
 
-  const handleDBConnect = async (syncEnabled: boolean) => {
+  const powerSync = useMemo(() => {
     const db = new PowerSyncDatabase({
-      schema: buildSchema(syncEnabled),
+      schema: buildSchema(isSyncEnabled),
       database: { dbFilename: "mealzen.db" },
     });
 
-    try {
-      await db.init();
-    } catch (error) {
-      Toast.show({
-        type: "error",
-        text1: "PowerSyncDatabase init error",
-        text2: parseError(error),
-      });
-      Logger.error("PowerSyncDatabase init error", error);
-    }
+    db.init();
 
-    try {
-      if (syncEnabled) {
-        await db.connect(connector);
-        Logger.log("connected", db.connected);
-      }
-    } catch (error) {
-      Toast.show({
-        type: "error",
-        text1: `PowerSyncDatabase ${syncEnabled ? "connection" : "disconnect"} error`,
-        text2: String(error),
-      });
-      Logger.error("PowerSyncDatabase connection error", error);
-    }
-
-    setStatus(db.currentStatus);
-    setPowersync(db);
-  };
+    return db;
+  }, []);
 
   useEffect(() => {
     // connect/disconnect from supabase when sync is enabled/disabled
-    handleDBConnect(isSyncEnabled);
-  }, [isSyncEnabled]);
-
-  useEffect(() => {
-    if (powerSync) {
-      // getting status directly from powerSync hook leads to stale data
-      // so we listen to status changes and assign to state
-      powerSync.registerListener({
-        statusChanged: setStatus,
+    if (isSyncEnabled) {
+      powerSync.connect(connector).then(() => {
+        Logger.log("connected", powerSync.connected);
       });
     }
-  }, [powerSync]);
 
-  if (powerSync === null || status === null) {
-    return <LoadingSplash />;
-  }
+    return () => {
+      if (powerSync.connected) {
+        powerSync.disconnect();
+      }
+    };
+  }, [powerSync, isSyncEnabled]);
 
   return <PowerSyncContext.Provider value={powerSync}>{children}</PowerSyncContext.Provider>;
 };

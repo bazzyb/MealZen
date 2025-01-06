29ffd8fe64c934a7d5e8f1edec19c9651841ab3a
Daryl Boakes <darylboakes@hotmail.co.uk>
2025-01-06 11:22:51 +0000

    add privacy policy link and app version

--- src/views/settings/components/Category.tsx
+++ src/views/settings/components/Category.tsx
@@ -10,12 +10,15 @@ export function Category({ children }: PropsWithChildren<object>) {
     <ViewRow
       borderBottomColor={colors.gray[5]}
       borderBottomWidth={1}
+      minHeight={2}
       paddingHorizontal={10}
       backgroundColor={colors.disabledLight}
     >
-      <Text bold style={{ color: colors.black, fontSize: 12 }}>
-        {children}
-      </Text>
+      {children && (
+        <Text bold style={{ color: colors.black, fontSize: 12 }}>
+          {children}
+        </Text>
+      )}
     </ViewRow>
   );
 }
--- src/views/settings/index.tsx
+++ src/views/settings/index.tsx
@@ -1,12 +1,17 @@
 import { router } from "expo-router";
+import DeviceInfo from "react-native-device-info";
 
 import { Text, ViewColumn } from "@/components";
+import { useAppTheme } from "@/styles/useAppTheme";
 
-// import { Auth } from "./Auth";
-// import { Category } from "./components/Category";
+import { Category } from "./components/Category";
 import { MenuItem } from "./components/MenuItem";
 
+// import { Auth } from "./Auth";
+
 export default function SettingsView() {
+  const { colors } = useAppTheme();
+
   return (
     <ViewColumn>
       {/* {__DEV__ && (
@@ -19,10 +24,26 @@ export default function SettingsView() {
       <MenuItem role="link" onPress={() => router.navigate("/(settings)/books")} style={{ paddingVertical: 4 }}>
         <Text>Books</Text>
       </MenuItem>
+      <Category />
+      <MenuItem
+        role="link"
+        onPress={() => router.navigate("https://www.mealzen.co.uk/privacy-policy")}
+        style={{ paddingVertical: 4 }}
+      >
+        <Text>Privacy Policy</Text>
+      </MenuItem>
+      <MenuItem style={{ paddingVertical: 6, minHeight: "auto" }}>
+        <Text style={{ fontSize: 12 }} color={colors.textSecondary}>
+          App Version: {DeviceInfo.getVersion()}
+        </Text>
+      </MenuItem>
       {__DEV__ && (
-        <MenuItem role="link" onPress={() => router.navigate("/dev-settings")} style={{ paddingVertical: 4 }}>
-          <Text>Dev Settings</Text>
-        </MenuItem>
+        <>
+          <Category />
+          <MenuItem role="link" onPress={() => router.navigate("/dev-settings")} style={{ paddingVertical: 4 }}>
+            <Text>Dev Settings</Text>
+          </MenuItem>
+        </>
       )}
     </ViewColumn>
   );

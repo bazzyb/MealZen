c7ed90350edebefb273a29c1175265bc91b5a5dc
Daryl Boakes <darylboakes@hotmail.co.uk>
2025-02-05 19:46:52 +0000

    fix collection icon colours

--- src/views/collection/index.tsx
+++ src/views/collection/index.tsx
@@ -3,25 +3,28 @@ import { router } from "expo-router";
 
 import { Text, ViewColumn, ViewRow } from "@/components";
 import { MenuItem } from "@/components/MenuItem";
+import { useAppTheme } from "@/styles/useAppTheme";
 
 export default function CollectionView() {
+  const { colors } = useAppTheme();
+
   return (
     <ViewColumn>
       <MenuItem role="link" onPress={() => router.navigate("/(collection)/meals")}>
         <ViewRow alignItems="center" gap={8}>
-          <MaterialCommunityIcons name="silverware-fork-knife" size={24} />
+          <MaterialCommunityIcons name="silverware-fork-knife" size={24} color={colors.text} />
           <Text>Meals</Text>
         </ViewRow>
       </MenuItem>
       <MenuItem role="link" onPress={() => router.navigate("/(collection)/books")}>
         <ViewRow alignItems="center" gap={8}>
-          <Entypo name="book" size={24} />
+          <Entypo name="book" size={24} color={colors.text} />
           <Text>Books</Text>
         </ViewRow>
       </MenuItem>
       <MenuItem role="link" onPress={() => router.navigate("/(collection)/cuisines")}>
         <ViewRow alignItems="center" gap={8}>
-          <MaterialCommunityIcons name="chef-hat" size={24} />
+          <MaterialCommunityIcons name="chef-hat" size={24} color={colors.text} />
           <Text>Cuisines</Text>
         </ViewRow>
       </MenuItem>
